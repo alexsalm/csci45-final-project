dc9d2eac7c2948f1a2659b9aa84585ddff47f08a
alexsalm <154659521+alexsalm@users.noreply.github.com>
2025-11-09 19:42:21 -0800

    Initialize document and add click events to input and buttons

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const planList = document.getElementById("planList");
 const planCount = document.getElementById("planCount");
 const addButton = document.getElementById("btn");
 const deleteButton = document.getElementById("deleteButton");
+
+// Initialize
+document.addEventListener("DOMContentLoaded", function () {
+    addButton.addEventListener("click", addTask);
+    planInput.addEventListener('keydown', function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click", deleteAllTasks);
+});
+
+function addTask() {
+    // some logic
+}
+
+function deleteAllTasks() {
+    // some logic
+}
